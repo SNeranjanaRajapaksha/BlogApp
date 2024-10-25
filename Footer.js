170a4c777004faec6276909efeb293e4ea7ccadb
NeranjanaRajapaksha <161111175+SNeranjanaRajapaksha@users.noreply.github.com>
2024-10-25 23:43:16 +0530

    initial commit

---
+++ Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../img/logo.png'
+
+export const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="footer" />
+      <span>All rights reserved</span>
+    </footer>
+  )
+}
+export default Footer
